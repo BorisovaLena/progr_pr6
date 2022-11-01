25539486ae6c7b4537849f7f54c99c680bda9cad
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-11-01 12:00:41 +0300

    28.10.22

---
+++ ПР6/page/PageAddUpdate.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ПР6.page
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddUpdate.xaml
+    /// </summary>
+    public partial class PageAddUpdate : Page
+    {
+        public PageAddUpdate()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.mainFrame.Navigate(new PageTovari());
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
